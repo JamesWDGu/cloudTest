ac6170119f79a972f9c554a39367f6ef455ec16f
James Gu <jameswdgu@gmail.com>
2022-06-10 20:24:33 +0800

    Initial Commit

---
+++ cloud/cloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudApp.swift
+//  cloud
+//
+//  Created by Weidong Gu on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct cloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
